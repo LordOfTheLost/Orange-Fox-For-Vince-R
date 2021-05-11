e8f6269de178cd554c3e8bf9a42dc65a3b49c44a
LordOfTheLost <48102549+LordOfTheLost@users.noreply.github.com>
2021-05-12 00:39:16 +0300

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2017 The Android Open Source Project
-# Copyright (C) 2017 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator 
+# Copyright 2021 The Android Open Source Project
+#
+# Copyright (C) 2019-2021 OrangeFox Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,56 +36,103 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_BOARD_SUFFIX := _64
 TARGET_USES_64_BIT_BINDER := true
 
+# ENABLE_CPUSETS := true
+# ENABLE_SCHEDBOOST := true
+
+# Bootloader
+# TARGET_BOOTLOADER_BOARD_NAME := msm8953
+# TARGET_NO_BOOTLOADER := true
+# TARGET_USES_UEFI := true
+
+# lzma
+# LZMA_RAMDISK_TARGETS := recovery
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_SECOND_OFFSET := 0x00f00000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-TARGET_PREBUILT_KERNEL := device/xiaomi/vince/prebuilt/Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_ARCH := arm64
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := vince
+# TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
 # Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 67108864
 BOARD_CACHEIMAGE_PARTITION_SIZE := 402653184
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 58846064640
-
-# File systems
-BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Workaround for error copying vendor files to recovery ramdisk
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Recovery
+TARGET_OTA_ASSERT_DEVICE := vince
+BOARD_HAS_LARGE_FILESYSTEM := true
+# BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Android Verified Boot
+# BOARD_AVB_ENABLE := false
+# BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+
+# exFAT FS Support
+TW_INCLUDE_FUSE_EXFAT := true
+
+# NTFS Support
+TW_INCLUDE_FUSE_NTFS := true
+
 # Crypto
-TARGET_HW_DISK_ENCRYPTION := true
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_EXTRA_LANGUAGES := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
-TW_SCREEN_BLANK_ON_BOOT := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+# TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+# TW_DEFAULT_BRIGHTNESS := 420
+TW_DEFAULT_LANGUAGE := en
+# TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXTRA_LANGUAGES := true
+# TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
+# TW_MAX_BRIGHTNESS := 1023
+PLATFORM_SECURITY_PATCH := 2099-12-31
+# TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+# AB_OTA_UPDATER := false
+TW_USE_QCOM_HAPTICS_VIBRATOR := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_USE_TOOLBOX := true
+
+# Extras
+TW_IGNORE_MISC_WIPE_DATA := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Debug
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
